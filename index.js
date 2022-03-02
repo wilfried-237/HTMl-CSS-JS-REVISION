aa1e88dc371b7431ee2d596e2d970e4cb7185599
wilfried-237 <you@example.com>
2022-03-02 21:33:58 +0100

    JS Part 1

---
+++ index.js
@@ -0,0 +1,101 @@
+// IF ELSE REVIVION
+/*
+var a = 2;
+
+if(a<=9){
+   alert('less than 9');
+}
+else{
+    alert('more than 9');
+}
+*/
+
+/*
+var a = 7;
+
+// SWITCH REVISION
+
+switch(a){
+    case 1: alert('this is case 1');
+    break;
+
+    case 2: alert('this is case 2');
+    break;
+
+    case 3: alert('this is case 3');
+    break;
+
+    case 4: alert('this is case 4');
+    break;
+
+    case 5: alert('this is case 5');
+    break;
+
+    default: alert('this is a default case')
+
+}
+*/
+
+// WHILE LOOP REVISION
+
+/*
+var a = 1;
+
+while(a<=10){
+    document.write('This is a while loop number '+a+'<br>');
+    a++;
+}
+*/
+
+// FOR LOOP REVISION
+
+/*
+var a;
+
+for(a=7;a<=35;a++){
+    document.write('This is a for loop number '+a+'<br>');
+}
+*/
+
+// ARRAY REVISION
+
+/*
+var a = [2,"cat",7,"rat",'dog']
+
+document.write(a[2])
+
+*/
+
+// SELF-DEFINED FUNCTION REVISION
+/*
+function function1(name,action) {
+    document.write("Hey "+name+" let go and "+action)
+}
+
+function1('Barry','play');
+
+*/
+
+// ANONYMOUS FUNCTIONS REVISION
+/*
+function() {
+    alert('This is an anonymous function')
+}
+*/
+
+// ARRAY PROPERTIED & METHODS REVISION
+/*
+var a = ['cat',7,9,3,6,7]
+
+a.pop() = removes the last element of the Array
+
+a.push(new value) = adds a new value at the end of the Array
+
+a.shift() = removes the first element of the Array
+
+a.unshift = adds an element at the begining of the array
+
+a.indexOf = to know the index of an element in an array
+*/
+
+// 
